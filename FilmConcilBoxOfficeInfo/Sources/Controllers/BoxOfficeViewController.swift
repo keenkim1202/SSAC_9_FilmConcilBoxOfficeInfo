3f14f717e624bc3b8a05f0c9947323a87a6f610e
KEEN <homurahomu@naver.com>
2021-11-03 12:14:16 +0900

    realm 적용

--- FilmConcilBoxOfficeInfo/Sources/Controllers/BoxOfficeViewController.swift
+++ FilmConcilBoxOfficeInfo/Sources/Controllers/BoxOfficeViewController.swift
@@ -9,44 +9,49 @@ import UIKit
 import Alamofire
 import SwiftyJSON
 import JGProgressHUD
+import RealmSwift
 
 // TODO: APIKEY.plist에 APIKEY를 입력해주세요.
 
+// TODO: 20031120 이전은 검색이 안됨. 데이터가 없나? -> 없으면 에러처리할 것.
+
 class BoxOfficeViewController: UIViewController {
   
-  // MARK: Properties
-  let progress = JGProgressHUD()
-  var boxOfficeData: [Movie] = [] {
+  // MARK: - Properties
+  let localRealm = try! Realm()
+  let movieList = List<Movie>()
+  
+  var tasks: Results<MovieList>!
+  var boxOfficeData: MovieList? {
     didSet {
-      if !boxOfficeData.isEmpty {
-        emptyLabel.textColor = .clear
-      } else {
-        emptyLabel.textColor = .systemYellow
-      }
       rankTableView.reloadData()
     }
   }
   
+  let progress = JGProgressHUD()
   let dateFormatter: DateFormatter = {
     let df = DateFormatter()
     df.locale = Locale(identifier: "ko-KR")
     df.dateFormat = "YYYYMMdd"
-    
     return df
   }()
   
-  // MARK: UI
+  // MARK: - UI
   @IBOutlet weak var searchTextField: UITextField!
   @IBOutlet weak var rankTableView: UITableView!
   @IBOutlet weak var emptyLabel: UILabel!
   
-  // MARK: View Life-Cycle
+  // MARK: - View Life-Cycle
   override func viewDidLoad() {
     super.viewDidLoad()
     configure()
+    
+    self.tasks = localRealm.objects(MovieList.self)
+    print("Realm Location: ", localRealm.configuration.fileURL)
+    print("tasks: \(tasks)")
   }
   
-  // MARK: Configure
+  // MARK: - Configure
   func configure() {
     rankTableView.delegate = self
     rankTableView.dataSource = self
@@ -56,11 +61,20 @@ class BoxOfficeViewController: UIViewController {
     searchTextField.layer.addBorder([.bottom], color: .white, width: 2)
   }
   
-  // MARK: Alert
+  func getTargetDate() -> String {
+    guard let text = searchTextField.text else { return "" }
+    
+    let calendar = Calendar.current
+    let yesterday = calendar.date(byAdding: .day, value: -1, to: dateFormatter.toDate(date: text)!)
+    return dateFormatter.toString(date: yesterday!)
+  }
+  
+  // MARK: - Alert
   fileprivate func showAlert(_ message: String) {
     UIAlertController.show(self, contentType: .error, message: message)
   }
   
+  // MARK: - Validation
   func isTextValidationSuccess(_ text: String) -> Bool {
     if text.isEmpty { // 검색어 유무
       showAlert("검색날짜가 비어있어요!\n작성 후 시도해주세요")
@@ -95,34 +109,70 @@ class BoxOfficeViewController: UIViewController {
     return true
   }
   
-  // MARK: Action
+  
+  // MARK: - Fetch Data
+  func isFetcehdBefore(searchDate: String) -> Bool {
+    self.boxOfficeData = localRealm.object(ofType: MovieList.self, forPrimaryKey: searchDate)
+    return self.boxOfficeData == nil ? false : true
+  }
+  
+  func fetchMovieInfo() {
+    let targetDate = getTargetDate()
+    
+    if isFetcehdBefore(searchDate: targetDate) {
+      print("FETCH - fetch된적 있음.")
+      let specificMovieList = localRealm.object(ofType: MovieList.self, forPrimaryKey: targetDate)
+      
+      self.boxOfficeData = specificMovieList
+      rankTableView.reloadData()
+    } else {
+      print("FETCH - fetch된적 없음.")
+      APIService.shared.fetchMovieInfo(targetDate) { code, json in
+        
+        let dailyBoxOfficeList = json["boxOfficeResult"]["dailyBoxOfficeList"].arrayValue
+        
+        dailyBoxOfficeList.forEach {
+          let rank = $0["rank"].intValue
+          let movieName = $0["movieNm"].stringValue
+          let openDate = $0["openDt"].stringValue
+          
+          let task = Movie(rank: rank, name: movieName, openDate: openDate)
+          self.movieList.append(task)
+        }
+        self.boxOfficeData = MovieList(movies: self.movieList, searchDate: targetDate)
+        
+        try! self.localRealm.write {
+          self.localRealm.add(self.boxOfficeData!)
+        }
+      }
+    }
+    emptyLabel.textColor = .clear
+  }
+  
+  
+  // MARK: - Action
   @IBAction func onSearch(_ sender: UIButton) {
-    boxOfficeData = []
     guard let text = searchTextField.text else { return }
+    self.boxOfficeData = nil
+    self.movieList.removeAll()
     
     if isTextValidationSuccess(text) {
       self.view.endEditing(true)
       progress.show(in: view, animated: true)
       
-      let calendar = Calendar.current
-      let yesterday = calendar.date(byAdding: .day, value: -1, to: dateFormatter.toDate(date: text)!)
-      let targetDate = dateFormatter.toString(date: yesterday!)
-    
-      APIService.shared.fetchMovieInfo(targetDate) { code, json in
-        
-        let dailyBoxOfficeList = json["boxOfficeResult"]["dailyBoxOfficeList"]
-        
-        _ = dailyBoxOfficeList.map {
-          let rank = $0.1["rank"].intValue
-          let movieName = $0.1["movieNm"].stringValue
-          let openDate = $0.1["openDt"].stringValue
-          self.boxOfficeData.append(Movie(rank: rank, name: movieName, openDate: openDate))
+      fetchMovieInfo()
+      self.progress.dismiss(animated: true)
+      
+      if let data = boxOfficeData {
+        if data.movies.count == 0 {
+          emptyLabel.textColor = .systemYellow
+        } else {
+          emptyLabel.textColor = .clear
         }
       }
-      boxOfficeData.sort{ $0.rank < $1.rank }
-      self.progress.dismiss(animated: true)
     } else {
       searchTextField.text = ""
+      emptyLabel.textColor = .systemYellow
       emptyLabel.text = "입력 날짜에 대한 검색 결과가 없어요 ㅠ.ㅠ"
     }
   }
@@ -137,16 +187,25 @@ extension BoxOfficeViewController: UITableViewDelegate {
 
 extension BoxOfficeViewController: UITableViewDataSource {
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return boxOfficeData.count
+    if let data = boxOfficeData {
+      return data.movies.count
+    } else {
+      return 0
+    }
   }
   
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    print(#function)
     guard let cell = tableView.dequeueReusableCell(withIdentifier: BoxOfficeTableViewCell.identifier) as? BoxOfficeTableViewCell else { return UITableViewCell() }
-    let data = boxOfficeData[indexPath.row]
-    cell.rankLabel.text = "\(data.rank)"
-    cell.movieTitleLabel.text = data.name
-    cell.releaseDateLabel.text = data.openDate
-    cell.selectionStyle = .none
+    if let datas = boxOfficeData {
+      let data = datas.movies[indexPath.row]
+      
+      cell.rankLabel.text = "\(data.rank)"
+      cell.movieTitleLabel.text = data.name
+      cell.releaseDateLabel.text = data.openDate
+      cell.selectionStyle = .none
+    }
     return cell
   }
+  
 }
--- FilmConcilBoxOfficeInfo/Sources/Model/Movie.swift
+++ FilmConcilBoxOfficeInfo/Sources/Model/Movie.swift
@@ -6,9 +6,31 @@
 //
 
 import Foundation
+import RealmSwift
 
-struct Movie {
-  var rank: Int
-  var name: String
-  var openDate: String
+class Movie: Object {
+  @Persisted var rank: Int
+  @Persisted var name: String
+  @Persisted var openDate: String
+
+  convenience init(rank: Int, name: String, openDate: String) {
+    self.init()
+    
+    self.rank = rank
+    self.name = name
+    self.openDate = openDate
+  }
+}
+
+class MovieList: Object {
+  @Persisted var movies: List<Movie>
+  
+  @Persisted(primaryKey: true) var searchDate: String
+  
+  convenience init(movies: List<Movie>, searchDate: String) {
+    self.init()
+    
+    self.movies = movies
+    self.searchDate = searchDate
+  }
 }
